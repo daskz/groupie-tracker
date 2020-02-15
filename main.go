8c2693fed19da95375f06f5478a9fccaee8b8d80
DS <tdaskz@gmail.com>
2020-02-15 19:03:00 +0600

    added getting artists from api

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"text/template"
 )
 
+type Artist struct {
+	ID           int
+	Name         string
+	CreationDate int
+	FirstAlbum   string
+	Image        string
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/artists", artistsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -22,4 +33,27 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t.Execute(w, page)
+}
+
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	var artists []Artist
+
+	res, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(bodyBytes, &artists)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(artists)
 }
