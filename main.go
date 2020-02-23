4eedcf3cc04eda364cd13bdd3bb90c9d929174e9
DS <tdaskz@gmail.com>
2020-02-23 12:57:38 +0600

    added artist api

--- main.go
+++ main.go
@@ -19,9 +19,19 @@ type Artist struct {
 	Members      []string
 }
 
+type Relation struct {
+	Index []ArtistRelation
+}
+
+type ArtistRelation struct {
+	ID             int
+	DatesLocations map[string]interface{}
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/api/artists/", artistsHandler)
+	http.HandleFunc("/api/concerts/", concertsHandler)
 
 	fs := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
@@ -39,6 +49,31 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles(fileName)
 
 	t.Execute(w, nil)
+}
+
+func concertsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("/api/concerts requested")
+	res, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusOK {
+		relation := Relation{}
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		//log.Println(string(bodyBytes))
+		res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(bodyBytes, &relation)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		//1og.Println(relation)
+		json.NewEncoder(w).Encode(relation.Index)
+	}
 }
 
 func artistsHandler(w http.ResponseWriter, r *http.Request) {
@@ -75,6 +110,15 @@ func artistsHandler(w http.ResponseWriter, r *http.Request) {
 
 func filterArtistByID(artists []Artist, id int) *Artist {
 	for _, item := range artists {
+		if item.ID == id {
+			return &item
+		}
+	}
+	return nil
+}
+
+func filterRelationByID(relations []ArtistRelation, id int) *ArtistRelation {
+	for _, item := range relations {
 		if item.ID == id {
 			return &item
 		}
